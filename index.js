048c4793b15a2ec422e8f057ebbd9c93e94e54d6
Trevor Heins <trevor.heins@stvinc.com>
2015-10-13 12:05:34 -0400

    Fixed test script and loud file name bug

--- index.js
+++ index.js
@@ -18,16 +18,43 @@ exports.string = function (str, algorithm, encoding) {
         .update(str, 'utf8')
         .digest(encoding || 'hex');
 }
-exports.file = function(paths, algorithim, callback) {
 
+exports.file = function(paths, algorithim, callback) {
     loopThroughPaths(paths, algorithim, function(md5List){
         callback(md5List);
     });
+}
 
-
+exports.file.quiet = function(paths, algorithim, callback) {
+    loopThroughPathsQuiet(paths, algorithim, function(md5List){
+        callback(md5List);
+    });
 }
 
 function loopThroughPaths(paths, algorithim, callback)
+{
+    var md5s = [];
+    var fileNames = [];
+    var processedCounter = 0;
+    for(var i = 0; i < paths.length; i++)
+    {
+        var path = paths[i];
+        var pathSplit = path.split("/");
+        fileNames.push(pathSplit[pathSplit.length - 1]);
+        fileMd5(path, algorithim, function(md5) {
+            var md5Str = "MD5 (" + fileNames[processedCounter] + ")" + " = " + md5;
+            md5s.push(md5Str);
+            if (processedCounter + 1 == i)
+            {
+                callback(md5s);
+            }
+            ++processedCounter;
+        });
+
+    }
+}
+
+function loopThroughPathsQuiet(paths, algorithim, callback)
 {
     var md5s = [];
     var processedCounter = 0;
@@ -35,6 +62,7 @@ function loopThroughPaths(paths, algorithim, callback)
     {
         var path = paths[i];
         fileMd5(path, algorithim, function(md5) {
+
             md5s.push(md5);
             if (processedCounter + 1 == i)
             {
--- test/test.js
+++ test/test.js
@@ -1,7 +1,13 @@
 var md5 = require('../index.js');
 var assert = require('assert');
-md5.file(['./test/testfile.txt', './test/anotherTestFile.txt'] , 'md5', function(md) {
+md5.file.quiet(['./test/testfile.txt', './test/anotherTestFile.txt'] , 'md5', function(md) {
     assert.equal(md[0], "f0ffb8dbde05931dfb9baeee5c86b214");
     assert.equal(md[1], "59a8935a0ac35991c27f29d4f25ea8b9");
 });
+
+md5.file(['./test/testfile.txt', './test/anotherTestFile.txt'] , 'md5', function(md) {
+    assert.equal(md[0], "MD5 (testfile.txt) = f0ffb8dbde05931dfb9baeee5c86b214");
+    assert.equal(md[1], "MD5 (anotherTestFile.txt) = 59a8935a0ac35991c27f29d4f25ea8b9");
+});
+
 assert.equal(md5.string("Hello, world"), "bc6e6f16b8a077ef5fbc8d59d0b931b9");
